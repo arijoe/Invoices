765fe035fdc5ed36459803a04d2561d9435e440c
arijoe <ari.weitzman@gmail.com>
2015-10-11 23:58:29 -0700

    Conjoin unecessary files.

--- javascripts/content.js
+++ javascripts/content.js
@@ -8,3 +8,23 @@ data.line_items.forEach( function (item) {
     .data("price", item.price)
   );
 });
+
+$( "select" )
+  .change( function () {
+    var price = "";
+    $( "select option:selected" ).val( function() {
+      price = $( this ).data( "price" );
+    }
+  );
+
+    $("#price").val(price);
+  })
+  .change();
+
+  $( "#quantity" ).change( function () {
+      var quant = $( "#quantity" ).val();
+      var price = $( "#price" ).val();
+      $( "#total" ).val( "$" + (quant * price).toFixed(2) );
+    }
+  )
+  .change();
--- javascripts/price_match.js
+++ javascripts/price_match.js
@@ -1,19 +1,1 @@
-$( "select" )
-  .change( function () {
-    var price = "";
-    $( "select option:selected" ).val( function() {
-      price = $( this ).data( "price" );
-    }
-  );
 
-    $("#price").val(price);
-  })
-  .change();
-
-  $( "#quantity" ).change( function () {
-      var quant = $( "#quantity" ).val();
-      var price = $( "#price" ).val();
-      $( "#total" ).val( "$" + (quant * price).toFixed(2) );
-    }
-  )
-  .change();
