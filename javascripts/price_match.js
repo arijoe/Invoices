b4826bae08d824a37db33b3e6b34228c9d3466b1
arijoe <ari.weitzman@gmail.com>
2015-10-11 23:57:39 -0700

    Add auto field for price total.

--- javascripts/price_match.js
+++ javascripts/price_match.js
@@ -3,8 +3,17 @@ $( "select" )
     var price = "";
     $( "select option:selected" ).val( function() {
       price = $( this ).data( "price" );
-    });
+    }
+  );
 
     $("#price").val(price);
   })
+  .change();
+
+  $( "#quantity" ).change( function () {
+      var quant = $( "#quantity" ).val();
+      var price = $( "#price" ).val();
+      $( "#total" ).val( "$" + (quant * price).toFixed(2) );
+    }
+  )
   .change();
